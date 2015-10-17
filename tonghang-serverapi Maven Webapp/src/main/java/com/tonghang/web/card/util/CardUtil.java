ea1254aa437fb3af2f3baa6f4df9f928fd298b02
rpgmakervx <583110127@qq.com>
2015-10-17 10:45:21 +0800

    交换换名片小改动

--- tonghang-serverapi Maven Webapp/src/main/java/com/tonghang/web/card/util/CardUtil.java
+++ tonghang-serverapi Maven Webapp/src/main/java/com/tonghang/web/card/util/CardUtil.java
@@ -12,16 +12,20 @@ public class CardUtil {
 	public Map<String,Object> cardToMapConvertor(Card card){
 		Map<String,Object> cardmap = new HashMap<String, Object>();
 		Map<String,Object> result = new HashMap<String, Object>();
-		cardmap.put("companyname", card.getCompanyname());
-		cardmap.put("realname", card.getRealname());
-		cardmap.put("position", card.getPosition());
-		cardmap.put("work_date", card.getWork_date());
-		cardmap.put("email", card.getEmail());
-		cardmap.put("phone", card.getPhone());
-		cardmap.put("schoolname", card.getSchoolname());
-		cardmap.put("major", card.getMajor());
-		cardmap.put("diploma", card.getDiploma());
-		cardmap.put("school_date", card.getSchool_date());
+		if(card!=null){
+			cardmap.put("companyname", card.getCompanyname());
+			cardmap.put("realname", card.getRealname());
+			cardmap.put("position", card.getPosition());
+			cardmap.put("work_date", card.getWork_date());
+			cardmap.put("email", card.getEmail());
+			cardmap.put("phone", card.getPhone());
+			cardmap.put("schoolname", card.getSchoolname());
+			cardmap.put("major", card.getMajor());
+			cardmap.put("diploma", card.getDiploma());
+			cardmap.put("school_date", card.getSchool_date());
+			cardmap.put("exchange_times", card.getExchange_times());
+			cardmap.put("chat_times", card.getChat_times());
+		}
 		result.put("card", cardmap);
 		return result;
 	}
